e6c7e67eafc18c446b9f28671176fc64fbe81d31
Eve0119 <gerictorilla@gmail.com>
2025-05-22 16:51:41 +0800

    added props

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,7 +1,11 @@
-export const Greet = () => {
+interface GreetProps {
+  name: string;
+}
+
+export const Greet = (props: GreetProps) => {
   return (
     <div>
-      <h2>Welcome Eve! You have 10 unread messages</h2>
+      <h2>Welcome {props.name}! You have 10 unread messages</h2>
     </div>
   );
 };
