4598d9330a1b6360279b94e85734dc9fd23ce06e
Eve0119 <gerictorilla@gmail.com>
2025-05-23 10:15:05 +0800

    personList

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import "./App.css";
 import { Greet } from "./components/Greet";
 import Person from "./components/Person";
+import PersonList from "./components/PersonList";
 
 function App() {
   const personName = {
     first: "Eve",
     last: "Laraya",
   };
+
+  const nameList = [
+    {
+      first: "Eve",
+      last: "Laraya",
+    },
+    {
+      first: "Cody",
+      last: "Metcalfe",
+    },
+    {
+      first: "Kayla",
+      last: "Foster",
+    },
+  ];
   return (
     <div className="App">
       <Greet name="hi" messageCount={20} />
       <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
