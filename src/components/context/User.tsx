515a2cc49ec4991749bb9ea4c7a00a80b425cb55
Eve0119 <gerictorilla@gmail.com>
2025-05-27 14:05:24 +0800

    improve code

--- src/components/context/User.tsx
+++ src/components/context/User.tsx
@@ -4,24 +4,20 @@ import { UserContext } from "./UserContext";
 export const User = () => {
   const userContext = useContext(UserContext);
   const handleLogin = () => {
-    if (userContext) {
-      userContext.setUser({
-        name: "Eve",
-        email: "eve_example@email.com",
-      });
-    }
+    userContext.setUser({
+      name: "Eve",
+      email: "eve_example@email.com",
+    });
   };
   const handleLogout = () => {
-    if (userContext) {
-      userContext.setUser(null);
-    }
+    userContext.setUser(null);
   };
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
       <button onClick={handleLogout}>LogOut</button>
-      <div>User name is {userContext?.user?.name}</div>
-      <div>User email is {userContext?.user?.email}</div>
+      <div>User name is {userContext.user?.name}</div>
+      <div>User email is {userContext.user?.email}</div>
     </div>
   );
 };
