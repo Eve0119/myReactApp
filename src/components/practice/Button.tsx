b63ac6642bd7d2638ac669281f7004c47e935b05
Eve0119 <gerictorilla@gmail.com>
2025-05-28 08:52:54 +0800

    useEffect

--- src/components/practice/Button.tsx
+++ src/components/practice/Button.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const Button = () => {
   const [count, setCount] = useState(0);
@@ -8,6 +8,14 @@ export const Button = () => {
   const handleDecrement = () => {
     setCount(count - 1);
   };
+
+  useEffect(() => {
+    console.log("Count Being Changed into: " + count);
+    return () => {
+      console.log("Count Being Cleared!");
+    };
+  }, [count]);
+
   return (
     <>
       <button onClick={handleIncrement}>+</button>
